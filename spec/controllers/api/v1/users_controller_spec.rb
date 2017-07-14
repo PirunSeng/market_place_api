f5fe99a23fa43ef3467a6f9d1df228b987c222e1
PirunSeng <sengpirun.rain@gmail.com>
2017-07-14 21:51:10 +0700

    Refactors test headers for each request

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -1,10 +1,8 @@
 describe Api::V1::UsersController do
-  before(:each) { request.headers['Accept'] = "application/vnd.marketplace.v1" }
-
   describe "GET #show" do
     before(:each) do
       @user = FactoryGirl.create :user
-      get :show, id: @user.id, format: :json
+      get :show, id: @user.id
     end
 
     it "returns the information about a reporter on a hash" do
@@ -19,7 +17,7 @@ describe Api::V1::UsersController do
     context 'when is successfully created' do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for :user
-        post :create, { user: @user_attributes }, format: :json
+        post :create, { user: @user_attributes }
       end
 
       it 'renders the json representation for the user record just created' do
@@ -33,7 +31,7 @@ describe Api::V1::UsersController do
     context 'when is not created' do
       before(:each) do
         @invalid_user_attributes = { password: '12345678', password_confirmation: '12345678' }
-        post :create, { user: @invalid_user_attributes }, format: :json
+        post :create, { user: @invalid_user_attributes }
       end
 
       it 'renders an errors json' do
@@ -54,7 +52,7 @@ describe Api::V1::UsersController do
     context 'when is successfully updated' do
       before(:each) do
         @user = FactoryGirl.create(:user)
-        patch :update, { id: @user.id, user: { email: 'newemail@example.com' }, format: :json }
+        patch :update, { id: @user.id, user: { email: 'newemail@example.com' } }
       end 
 
       it 'renders the json representation for the updated user' do
@@ -68,7 +66,7 @@ describe Api::V1::UsersController do
     context 'when is not updated' do
       before(:each) do
         @user = FactoryGirl.create(:user)
-        patch :update, { id: @user.id, user: { email: 'example.com' } }, format: :json
+        patch :update, { id: @user.id, user: { email: 'example.com' } }
       end
 
       it 'renders an errors json' do
@@ -89,7 +87,7 @@ describe Api::V1::UsersController do
     context 'when is successfully destroyed' do
       before(:each) do
         @user = FactoryGirl.create(:user)
-        delete :destroy,  { id: @user.id }, format: :json
+        delete :destroy,  { id: @user.id }
       end
 
       it { should respond_with 204 }
