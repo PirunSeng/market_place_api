43b5bc0def05ec4699f739c9c5a295f0e9659c0a
PirunSeng <sengpirun.rain@gmail.com>
2017-07-17 23:13:44 +0700

    Adds products create update and destroy action nested on users

--- spec/controllers/api/v1/products_controller_spec.rb
+++ spec/controllers/api/v1/products_controller_spec.rb
@@ -29,4 +29,89 @@ describe Api::V1::ProductsController do
 
     it { should respond_with 200 }
   end
+
+  describe 'POST #create' do
+    context "when is successfully created" do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @product_attributes = FactoryGirl.attributes_for :product
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, product: @product_attributes }
+      end
+
+      it 'renders json represent to the post product' do
+        product_response = json_response
+        expect(json_response[:title]).to eq(@product_attributes[:title])
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        user = FactoryGirl.create :user
+        @invalid_product_attributes = { title: FFaker::Product.product_name, price: 'Invalid price' }
+        api_authorization_header user.auth_token
+        post :create, { user_id: user.id, product: @invalid_product_attributes }
+      end
+
+      it 'renders json errors' do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+
+      it 'renders the json errors on why the product could not be created' do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include('is not a number')
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
+  describe 'PUT/PATCH #update' do
+    before(:each) do
+      @user = FactoryGirl.create :user
+      @product = FactoryGirl.create(:product, user_id: @user.id)
+      api_authorization_header @user.auth_token
+    end
+    context 'successfully updated' do
+      before(:each) do
+        patch :update, { user_id: @user.id, id: @product.id, product: { title: 'Apple' } }
+      end
+      it 'renders json represent updated product' do
+        product_response = json_response
+        expect(product_response[:title]).to eq('Apple')
+      end
+      it { should respond_with 201 }
+    end
+
+    context 'could not update' do
+      before(:each) do
+        patch :update, { user_id: @user.id, id: @product.id, product: { price: 'Invalid price' } }
+      end
+      it 'render json errors' do
+        product_response = json_response
+        expect(product_response).to have_key(:errors)
+      end
+      it 'renders json errors why the product could not be updated' do
+        product_response = json_response
+        expect(product_response[:errors][:price]).to include('is not a number')
+      end
+      it { should respond_with 422 }
+    end
+  end
+
+  describe 'DELETE #destroy' do
+    context 'successfully destroyed' do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        @product = FactoryGirl.create :product, user_id: @user.id
+        api_authorization_header @user.auth_token
+        delete :destroy, { user_id: @user.id, id: @product.id }
+      end
+
+      it { should respond_with 204 }
+    end
+  end
 end
