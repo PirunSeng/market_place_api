bc5de0b6ba45d638cdb5cb41e8840405c66eed22
PirunSeng <sengpirun.rain@gmail.com>
2017-07-21 23:40:33 +0700

    Adds some filters to Product

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -14,4 +14,78 @@ describe Product do
   it { should validate_presence_of(:user_id) }
 
   it { should belong_to(:user) }
+
+  describe '.filter_by_title' do
+    let!(:product_a){ create(:product, title: 'A plasma TV') }
+    let!(:product_b){ create(:product, title: 'Fastest Laptop') }
+    let!(:product_c){ create(:product, title: 'CD player') }
+    let!(:product_d){ create(:product, title: 'LCD TV') }
+
+    context "when a 'TV' title pattern is sent" do
+      it "returns the 2 products matching" do
+        expect(Product.filter_by_title("TV").size).to eq(2)
+      end
+
+      it "returns the products matching" do
+        expect(Product.filter_by_title("TV").sort).to match_array([product_a, product_d])
+      end
+    end
+  end
+
+  describe ".above_or_equal_to_price" do
+    let!(:product_a){ create(:product, price: 100) }
+    let!(:product_b){ create(:product, price: 50) }
+    let!(:product_c){ create(:product, price: 150) }
+
+    it "returns the products which are above or equal to the price" do
+      expect(Product.above_or_equal_to_price(100).sort).to match_array([product_a, product_c])
+    end
+  end
+
+  describe '.below_or_equal_to_price' do
+    let!(:product_a){ create(:product, price: 100) }
+    let!(:product_b){ create(:product, price: 50) }
+    let!(:product_c){ create(:product, price: 150) }
+
+    it "returns the products which are below or equal to the price" do
+      expect(Product.below_or_equal_to_price(100).sort).to match_array([product_b, product_a])
+    end
+  end
+
+  describe '.recent' do
+    let!(:product_a){ create(:product) }
+    let!(:product_b){ create(:product) }
+    let!(:product_c){ create(:product) }
+
+    it 'returns the products in reverse chronological order' do
+      expect(Product.recent).to match_array([product_a, product_c, product_b])
+    end
+  end
+
+  describe '.search' do
+    let!(:product_a){ create(:product, price: 100, title: "Plasma tv") }
+    let!(:product_b){ create(:product, price: 50, title: "Videogame console") }
+    let!(:product_c){ create(:product, price: 150, title: "MP3") }
+    let!(:product_d){ create(:product, price: 99, title: "Laptop") }
+
+    context "when title 'videogame' and '100' a min price are set" do
+      it "returns an empty array" do
+        search_hash = { keyword: "videogame", min_price: 100 }
+        expect(Product.search(search_hash)).to be_empty
+      end
+    end
+
+    context "when title 'tv', '150' as max price, and '50' as min price are set" do
+      it "returns the product1" do
+        search_hash = { keyword: "tv", min_price: 50, max_price: 150 }
+        expect(Product.search(search_hash)).to match_array([product_a]) 
+      end
+    end
+
+    context "when an empty hash is sent" do
+      it "returns all the products" do
+        expect(Product.search({})).to match_array([product_a, product_b, product_c, product_d])
+      end
+    end
+  end
 end
