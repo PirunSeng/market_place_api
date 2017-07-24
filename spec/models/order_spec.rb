888b6dfd08866a52510246b909831a9f4d9e8fd1
PirunSeng <sengpirun.rain@gmail.com>
2017-07-25 00:12:44 +0700

    Adds validator for order with not enough products on stock

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -28,8 +28,8 @@ describe Order do
 
   describe '#build_placements_with_product_ids_and_quantities' do
     before(:each) do
-      product_a = FactoryGirl.create(:product, price: 100, quantity: 5)      
-      product_b = FactoryGirl.create(:product, price: 85, quantity: 10)      
+      product_a = FactoryGirl.create(:product, price: 100, quantity: 5)
+      product_b = FactoryGirl.create(:product, price: 85, quantity: 10)
 
       @product_ids_and_quantities = [[product_a.id, 2], [product_b.id, 3]]
     end
@@ -37,6 +37,24 @@ describe Order do
     it 'builds 2 placements for the order' do
       expect{order.build_placements_with_product_ids_and_quantities(@product_ids_and_quantities)}.to change{order.placements.size}.from(0).to(2)
     end
+  end
 
+  describe '#valid?' do
+    before(:each) do
+      product_a = FactoryGirl.create(:product, price: 100, quantity: 5)
+      product_b = FactoryGirl.create(:product, price: 85, quantity: 10)
+
+      placement_a = FactoryGirl.build :placement, product: product_a, quantity: 3
+      placement_b = FactoryGirl.build :placement, product: product_b, quantity: 15
+
+      @order = FactoryGirl.build :order
+
+      @order.placements << placement_a
+      @order.placements << placement_b
+    end
+    
+    it 'becomes invalid due to insufficient products' do
+      expect(@order.valid?).to be_falsey
+    end
   end
 end
